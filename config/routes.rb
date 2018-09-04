f78bf157d8bb42a8ee22d0966529385918fda9be
Vivek <vivek.selvarajan@people10.com>
2018-09-04 19:16:02 +0530

    Body color changed and edit button added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
     resources :users, only: [:index, :create, :show , :new] 
       resources :posts do
         resources :comments
      end
     
   
-  root 'welcome#index'
+  # root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
